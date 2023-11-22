cbb5e9e2e1361db31c7d3f6c3bae3dde6d42e220
Adastephen <adakolestephen@gmail.com>
2023-11-22 17:16:38 +0100

    assinment

---
+++ first.js
@@ -0,0 +1,23 @@
+// document.getElementById('demo1').innerHTML = "My name is stehen";
+// let animal = prompt('your animal name', ' ');
+// function myanimal(animal){
+//    return animal + " " + "is my pet animal"
+// }
+// document.getElementById('demo2').innerHTML = myanimal();
+
+function add7(x){
+    return x + 7
+}
+function multiply(x, y) {
+    return x * y
+}
+function capitalize(a) {
+    return a.toUpperCase()
+}
+function lastLetter(b) {
+    return b.slice(-1)
+}
+console.log(add7(3));
+console.log(multiply(4, 5));
+console.log(capitalize("today"));
+console.log(lastLetter("cachess"));
