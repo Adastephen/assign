5b35f7d6c18a2224e6e4b18775e8f17efd704c9f
Adastephen <adakolestephen@gmail.com>
2023-11-22 18:22:14 +0100

    assign

--- first.js
+++ first.js
@@ -5,19 +5,37 @@
 // }
 // document.getElementById('demo2').innerHTML = myanimal();
 
-function add7(x){
-    return x + 7
-}
-function multiply(x, y) {
-    return x * y
-}
-function capitalize(a) {
-    return a.toUpperCase()
-}
-function lastLetter(b) {
-    return b.slice(-1)
-}
-console.log(add7(3));
-console.log(multiply(4, 5));
-console.log(capitalize("today"));
-console.log(lastLetter("cachess"));
+// function add7(x){
+//     return x + 7
+// }
+// function multiply(x, y) {
+//     return x * y
+// }
+// function capitalize(a) {
+//     return a.toUpperCase()
+// }
+// function lastLetter(b) {
+//     return b.slice(-1)
+// }
+// console.log(add7(3));
+// console.log(multiply(4, 5));
+// console.log(capitalize("today"));
+// console.log(lastLetter("cachess"));
+
+// let answer = prompt("enter you number: ");
+
+// for (let i = 0; i <= answer; i++) {
+    
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzbuzz");
+//     }
+//     else if (i % 3 === 0){
+//         console.log("fizz");
+//     }
+//     else if (i % 5 === 0){
+//         console.log("buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
